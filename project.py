6f3808d53fdf713536b7e86898455db1e76592df
“Brandon <Brandoon@pm.me>
2022-07-06 11:39:17 -0700

    Return clarification added

--- project.py
+++ project.py
@@ -1,27 +1,32 @@
-from project_func import check_url, green, red, blue, clear_screen, show_splash, savePath, read_save_path, download_video, download_playlist, choice1, choice3
-import sys
 import argparse as ap
 
-
+from project_func import choice1, choice3, clear_screen, savePath, show_splash
 
 # Youtube Offline Downloader
 
+
 def main():
     # get arguments from user using argparse module for only the url
-    parser = ap.ArgumentParser(description='Youtube Offline Downloader')
-    parser.add_argument('-u', '--url', help='URL of the video or playlist to download', required=False)
-    parser.add_argument('-p', '--path', help='Path to save the video or playlist', required=False)
+    parser = ap.ArgumentParser(description="Youtube Offline Downloader")
+    parser.add_argument(
+        "-u", "--url", help="URL of the video or playlist to download", required=False
+    )
+    parser.add_argument(
+        "-p", "--path", help="Path to save the video or playlist", required=False
+    )
     # playlist argument
-    parser.add_argument('-pl', '--playlist', help='URL of the playlist to download', required=False)
-    
+    parser.add_argument(
+        "-pl", "--playlist", help="URL of the playlist to download", required=False
+    )
+
     # if the url arg is provided, then download the video
     if parser.parse_args().url:
         choice1(parser.parse_args().url)
-    
+
     # if the path arg is provided, then set the path
     if parser.parse_args().path:
         savePath(parser.parse_args().path)
-    
+
     # if the playlist arg is provided, then download the playlist
     if parser.parse_args().playlist:
         choice3(parser.parse_args().playlist)
@@ -29,24 +34,23 @@ def main():
     # if no arguments are passed, show the splash screen
     clear_screen()
     show_splash()
-    print('1. Download video')
-    print('2. Default Download Location')
-    print('3. Download Playlist')
-    print('4. Exit')
-    
-    choice = input('Enter your choice: ')
-    if choice == '1':
+    print("1. Download video")
+    print("2. Default Download Location")
+    print("3. Download Playlist")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+    if choice == "1":
         choice1()
-    elif choice == '2':
+    elif choice == "2":
         savePath()
         main()
-    elif choice == '3':
+    elif choice == "3":
         choice3()
-    elif choice == '4':
-        print('Exiting...')
+    elif choice == "4":
+        print("Exiting...")
         exit(0)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- project_func.py
+++ project_func.py
@@ -9,59 +9,61 @@ from pyfiglet import Figlet
 def choice1(url: str = None):
     # download video
     if url is None:
-        url = input('Enter the url: ')
+        url = input("Enter the url : ")
     while check_url(url) is None:
-        url = input('Enter the url : ')
+        url = input("Enter the url : ")
     savedPath = read_save_path()
-    save_path = savedPath if savedPath != '' else savePath()
+    save_path = savedPath if savedPath != "" else savePath()
     download_video(url=url, save_path=save_path)
+
 
 def choice3(url: str = None):
     # download playlist
     if url is None:
-        url = input('Enter the url: ')
+        url = input("Enter the url: ")
     while check_url(url) is None:
-        url = input('Enter the url : ')
+        url = input("Enter the url : ")
     savedPath = read_save_path()
-    save_path = savedPath if savedPath != '' else savePath()
+    save_path = savedPath if savedPath != "" else savePath()
     download_playlist(url=url, save_path=save_path)
+
 
 def savePath(path: str = None):
     # User provided path to save the video
     if path is not None:
         save_path_to_file(path)
         clear_screen()
-        print(green('Saved new default path'))
+        print(green("Saved new default path"))
         sleep(1)
         return
     defaultPath = read_save_path()
     clear_screen()
     # return the path to save the video
-    print('Choose directory to save video:')
-    print('1. Current directory')
-    print('2. Downloads')
-    print('3. Documents')
-    print('4. Desktop')
-    print('5. Custom path')
-    print('6. Exit')
-    print('')
+    print("Choose directory to save video:")
+    print("1. Current directory")
+    print("2. Downloads")
+    print("3. Documents")
+    print("4. Desktop")
+    print("5. Custom path")
+    print("6. Exit")
+    print("")
     if defaultPath:
-        print(green(f'Current Default path = {defaultPath}'))
-    choice = input('Enter your choice: ')
-    path = ''
-    if choice == '1':
+        print(green(f"Current Default path = {defaultPath}"))
+    choice = input("Enter your choice: ")
+    path = ""
+    if choice == "1":
         path = os.getcwd()
-    elif choice == '2':
-        path = os.path.join(os.path.expanduser('~'), 'Downloads')
-    elif choice == '3':
-        path = os.path.join(os.path.expanduser('~'), 'Documents')
-    elif choice == '4':
-        path = os.path.join(os.path.expanduser('~'), 'Desktop')
-    elif choice == '5':
+    elif choice == "2":
+        path = os.path.join(os.path.expanduser("~"), "Downloads")
+    elif choice == "3":
+        path = os.path.join(os.path.expanduser("~"), "Documents")
+    elif choice == "4":
+        path = os.path.join(os.path.expanduser("~"), "Desktop")
+    elif choice == "5":
         # return the user chosen path to save the video
         clear_screen()
         path = userPath()
-    elif choice == '6':
+    elif choice == "6":
         return
     else:
         # re-ask the user to choose a directory
@@ -73,45 +75,44 @@ def savePath(path: str = None):
 
 def userPath():
     # Get user folder path and validate it
-    path = input('Enter the path: ')
+    path = input("Enter the path: ")
     if os.path.exists(path):
         return path
     else:
         clear_screen()
-        print(red('Path does not exist'))
+        print(red("Path does not exist"))
         userPath()
 
 
 def show_splash():
-    '''
+    """
     Display splash screen
-    '''
+    """
     clear_screen()
-    title = 'Y0utube Offline Downloader'
-    f = Figlet(font='standard')
+    title = "Y0utube Offline Downloader"
+    f = Figlet(font="standard")
     print(red(f.renderText(title)))
 
 
 def clear_screen():
-    print('\n' * 25)
+    print("\n" * 25)
 
 
 def save_path_to_file(path):
-    with open('save_path.txt', 'w') as f:
+    with open("save_path.txt", "w") as f:
         f.write(path)
 
 
-def read_save_path():
-    if os.path.exists('save_path.txt'):
-        with open('save_path.txt', 'r') as f:
+def read_save_path() -> str:
+    if os.path.exists("save_path.txt"):
+        with open("save_path.txt", "r") as f:
             return f.read()
-    return ''
+    return ""
 
 
 def check_url(url: str):
     # check if the url is valid
-    regex = (
-        r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.be)\/.+$")
+    regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.be)\/.+$"
 
     match = re.match(regex, url)
     if match:
@@ -122,38 +123,38 @@ def check_url(url: str):
 
 def green(text: str):
     # return text in green
-    return '\033[32m' + text + '\033[0m'
+    return "\033[32m" + text + "\033[0m"
 
 
 def red(text: str):
     # return text in red
-    return '\033[31m' + text + '\033[0m'
+    return "\033[31m" + text + "\033[0m"
 
 
 def blue(text: str):
     # return text in blue
-    return '\033[34m' + text + '\033[0m'
+    return "\033[34m" + text + "\033[0m"
 
 
 def download_video(url: str, save_path: str):
     # download youtube video and save it to a folder
     ydl_opts = {
         # hight quality video
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio',
+        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio",
         # save location of the video
-        'outtmpl': save_path + '/%(title)s.%(ext)s',
+        "outtmpl": save_path + "/%(title)s.%(ext)s",
     }
     youtube_dl.YoutubeDL(ydl_opts).extract_info(url)
 
 
 def download_playlist(url: str, save_path: str):
-    # download playlist from youtube using youtube-dl
+    """ download playlist from youtube using youtube-dl """
 
     ydl_opts = {
         # hight quality video
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio',
+        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio",
         # save location of the video
-        'outtmpl': save_path + '/%(title)s.%(ext)s',
-        'yes-playlist': True,
+        "outtmpl": save_path + "/%(title)s.%(ext)s",
+        "yes-playlist": True,
     }
     youtube_dl.YoutubeDL(ydl_opts).extract_info(url)
